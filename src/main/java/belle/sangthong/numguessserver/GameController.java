146fc3364dca4cf7497733c9285bbd646988c199
Belle Sangthong <belle.sangthong@iths.se>
2025-03-03 13:48:13 +0100

    Fixed controller and service

--- src/main/java/belle/sangthong/numguessserver/GameController.java
+++ src/main/java/belle/sangthong/numguessserver/GameController.java
@@ -1,8 +1,20 @@
 package belle.sangthong.numguessserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GameController {
 
+    @Autowired
+    GameService gameService;
+
+    @GetMapping("/guessnumber")
+    public String guessNumber(Model model) {
+        int number = gameService.getNumber();
+        model.addAttribute("number", number);
+        return "gamepage";
+    }
 }
--- src/main/java/belle/sangthong/numguessserver/GameService.java
+++ src/main/java/belle/sangthong/numguessserver/GameService.java
@@ -2,7 +2,14 @@ package belle.sangthong.numguessserver;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
 public class GameService {
 
+    AtomicInteger count = new AtomicInteger(0);
+
+    public int getNumber() {
+        return count.incrementAndGet();
+    }
 }
