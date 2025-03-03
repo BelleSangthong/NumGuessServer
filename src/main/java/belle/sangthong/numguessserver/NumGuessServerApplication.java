7e3c12988472179559de5d09715eec2de1d0136c
Belle Sangthong <belle.sangthong@iths.se>
2025-03-03 13:13:17 +0100

    Initial commit

---
+++ src/main/java/belle/sangthong/numguessserver/NumGuessServerApplication.java
@@ -0,0 +1,13 @@
+package belle.sangthong.numguessserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumGuessServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumGuessServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/belle/sangthong/numguessserver/NumGuessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package belle.sangthong.numguessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumGuessServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
